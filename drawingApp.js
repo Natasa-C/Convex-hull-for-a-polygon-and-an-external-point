0fa66d00a679208e1b8f1f50e557bf2b8918b6fa
madalinaC <cirstea_madalina_ionela@yahoo.com>
2019-12-31 15:15:58 +0200

    updated buttons features

--- drawingApp.js
+++ drawingApp.js
@@ -1,32 +1,44 @@
-window.onload = function() {
+window.onload = function () {
+    const canvas = document.querySelector("#drawingCanvas");
+    const newDrawingSession = document.querySelector("#newDrawingSession");
+    const addPoints = document.querySelector("#addPoints");
+    const addExternalPoint = document.querySelector("#addExternalPoint");
+    const drawPolygon = document.querySelector("#drawPolygon");
+
     const sideNavWidth = 257;
     const headerHeight = 104;
-
-    const canvas = document.querySelector("#drawingCanvas");
-    const draw = document.querySelector("#draw");
     const speed = 250;
     const r = 5;
     const cx = 5;
     const cy = 5;
-
+    let addPoint = false;
+    let addExtPoint = false;
     let points = [];
-    var interval;
 
-    canvas.onclick = function(event) {
+    newDrawingSession.onclick = function () {
+        clearCanvas(canvas);
+        points.length = 0;
+        addPoint = true;
+        addExtPoint = true;
+    }
+
+    canvas.onclick = function (event) {
         let x = event.clientX - sideNavWidth;
         let y = event.clientY - headerHeight;
 
-        points.push(new Point(x, y));
-
-        let newBox = document.createElement("div");
-        newBox.setAttribute("class", "circleBox");
-        newBox.style.left = x - cx + "px";
-        newBox.style.top = y - cy + "px";
-        newBox.innerHTML = `<svg id="circle"> <circle r="${5}" cx="5" cy="5"/> </svg>`;
-        canvas.appendChild(newBox);
+        if (addPoint) {
+            points.push(new Point(x, y));
+            drawPoint(canvas, x, y, r, cx, cy, "black");
+        }
+        else if (addExtPoint) {
+            let x = event.clientX - sideNavWidth;
+            let y = event.clientY - headerHeight;
+            drawPoint(canvas, x, y, r, cx, cy, "red");
+            addExtPoint = false;
+        }
     }
 
-    draw.onclick = async function() {
+    drawPolygon.onclick = async function () {
         for (let i = 0; i < points.length - 1; i++) {
             await timeout(speed);
             await drawLine(canvas, points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
@@ -34,6 +46,12 @@ window.onload = function() {
 
         await timeout(speed);
         await drawLine(canvas, points[points.length - 1].x, points[points.length - 1].y, points[0].x, points[0].y);
+
+        addPoint = false;
+    }
+
+    addExternalPoint.onclick = function () {
+        addPoint = false;
     }
 }
 
@@ -51,4 +69,18 @@ function drawLine(canvas, x1, y1, x2, y2) {
         canvas.innerHTML += `<svg id="lineBox"> <line id="lineStyle" x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}"/> </svg>`;
         resolve();
     });
+}
+
+function drawPoint(canvas, x, y, r, cx, cy, color) {
+    let newBox = document.createElement("div");
+    newBox.setAttribute("class", "circleBox");
+    newBox.style.left = x - cx + "px";
+    newBox.style.top = y - cy + "px";
+    newBox.innerHTML = `<svg id="circle"> <circle r="${r}" cx="${cx}" cy="${cy}" fill="${color}"/> </svg>`;
+    canvas.appendChild(newBox);
+}
+
+function clearCanvas(canvas) {
+    while (canvas.firstChild)
+        canvas.removeChild(canvas.firstChild);
 }
